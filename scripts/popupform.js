d4185a9a8cab4ad7828b4b158900bb19368173b7
Mark Lintern <mlintern@compendium.com>
2013-01-24 08:40:06 -0500

    Updated Imgages to PNG for 229 and bullet holes Created Clear mist option for popup winodw fixed styling for holes

--- scripts/popupform.js
+++ scripts/popupform.js
@@ -26,7 +26,7 @@ $(document).ready(function() {
 	// When clicking on the button close or the mask layer the popup closed
 	//$('#mask').live('click', function() { 
 	$('#mask').live("click touchstart", function(){
-		$('#mask , #popupform , #score-sheet , #scoreboard , #score-eucher','#bulletholes').fadeOut(300 , function() {
+		$('#mask , #popupform , #score-sheet , #scoreboard , #score-eucher').fadeOut(300 , function() {
 			$('#mask').remove();  
 		}); 
 		return false;
@@ -39,6 +39,40 @@ $(document).ready(function() {
 		}); 
 		return false;
 	});
+	
+	$('a.popup-link-clear').click(function() {
+
+		//Getting the variable's value from a link 
+		var popupBox = $(this).attr('href');
+
+		//Fade in the Popup
+		$(popupBox).fadeIn(300);
+
+		//Set the center alignment padding + border see css style
+		var popMargTop = ($(popupBox).height() + 24) / 2; 
+		var popMargLeft = ($(popupBox).width() + 24) / 2; 
+
+		$(popupBox).css({ 
+			'margin-top' : -popMargTop,
+			'margin-left' : -popMargLeft
+		});
+
+		// Add the mask to body
+		$('body').append('<div id="clearmask"></div>');
+		$('#clearmask').fadeIn(300);
+
+		return false;
+	});
+
+	// When clicking on the button close or the mask layer the popup closed
+	//$('#mask').live('click', function() { 
+	$('#clearmask').live("click touchstart", function(){
+		$('#clearmask , #bulletholes').fadeOut(300 , function() {
+			$('#clearmask').remove();  
+		}); 
+		return false;
+	});
+	
 });
 
 $(document).ready(function (){ 
