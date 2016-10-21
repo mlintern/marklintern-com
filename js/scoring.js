d35f4f729a1e6c108d4bf4809b0c6e2264d454e1
Mark Lintern <mark.lintern@oracle.com>
2016-10-21 10:18:21 -0400

    updated game score sheets to be more unified

--- js/scoring.js
+++ js/scoring.js
@@ -1,5 +1,12 @@
-//5 Crowns Scoring
+function isMobile() {
+	if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4)) ) {
+		return true;
+	} else {
+		return false;
+	}
+}
 
+//5 Crowns Scoring
 var array1=['11','21','31','41','51','61','71','81','91','101','j1','q1','k1'];
 var array2=['12','22','32','42','52','62','72','82','92','102','j2','q2','k2'];
 var array3=['13','23','33','43','53','63','73','83','93','103','j3','q3','k3'];
@@ -57,7 +64,7 @@ function reset_score(){
 }
 
 $(document).ready(function() {
-	$('#player_3').click(function() {
+	$('#player_3').on(isMobile() ? 'touchend' : 'click', function() {
 		$('.player3').show();
 		$('.player4').hide();
 		$('.player5').hide();
@@ -65,7 +72,7 @@ $(document).ready(function() {
 		$('.player7').hide();
 		$('.player8').hide();
 	});
-	$('#player_4').click(function() {
+	$('#player_4').on(isMobile() ? 'touchend' : 'click', function() {
 		$('.player3').show();
 		$('.player4').show();
 		$('.player5').hide();
@@ -73,7 +80,7 @@ $(document).ready(function() {
 		$('.player7').hide();
 		$('.player8').hide();
 	});
-	$('#player_5').click(function() {
+	$('#player_5').on(isMobile() ? 'touchend' : 'click', function() {
 		$('.player3').show();
 		$('.player4').show();
 		$('.player5').show();
@@ -81,7 +88,7 @@ $(document).ready(function() {
 		$('.player7').hide();
 		$('.player8').hide();
 	});
-	$('#player_6').click(function() {
+	$('#player_6').on(isMobile() ? 'touchend' : 'click', function() {
 		$('.player3').show();
 		$('.player4').show();
 		$('.player5').show();
@@ -89,7 +96,7 @@ $(document).ready(function() {
 		$('.player7').hide();
 		$('.player8').hide();
 	});
-	$('#player_7').click(function() {
+	$('#player_7').on(isMobile() ? 'touchend' : 'click', function() {
 		$('.player3').show();
 		$('.player4').show();
 		$('.player5').show();
@@ -97,7 +104,7 @@ $(document).ready(function() {
 		$('.player7').show();
 		$('.player8').hide();
 	});
-	$('#player_8').click(function() {
+	$('#player_8').on(isMobile() ? 'touchend' : 'click', function() {
 		$('.player3').show();
 		$('.player4').show();
 		$('.player5').show();
@@ -105,7 +112,7 @@ $(document).ready(function() {
 		$('.player7').show();
 		$('.player8').show();
 	});
-	$('#onetothree').click(function() {
+	$('#onetothree').on(isMobile() ? 'touchend' : 'click', function() {
 		if($('#onetothree').prop('checked')) {
 		    $("#rowone").show();
 		    $("#rowtwo").show();
@@ -114,7 +121,7 @@ $(document).ready(function() {
 		    $("#rowtwo").hide();
 		}
 	});
-	$('#jacktoking').click(function() {
+	$('#jacktoking').on(isMobile() ? 'touchend' : 'click', function() {
 		if($('#jacktoking').prop('checked')) {
 		    $("#rowJ").show();
 		    $("#rowQ").show();
@@ -129,37 +136,37 @@ $(document).ready(function() {
 
 //Scoreboard
 $(document).ready(function() {
-	$(".HomeMinus").click(function() {
-		var current = parseInt($("#homescore").val());
+	$(".HomeMinus").on(isMobile() ? 'touchend' : 'click', function() {
+		var current = parseInt($("#homescore").text());
 		if (current === 0){
 		}else{
-			$("#homescore").val(current - 1);
+			$("#homescore").text(current - 1);
 		}
 	});
-	$(".HomePlus").click(function() {
-		var current = parseInt($("#homescore").val());
+	$(".HomePlus").on(isMobile() ? 'touchend' : 'click', function() {
+		var current = parseInt($("#homescore").text());
 		if (current == 999){
 		}else{
-			$("#homescore").val(current + 1);
+			$("#homescore").text(current + 1);
 		}
 	});
-	$(".AwayMinus").click(function() {
-		var current = parseInt($("#awayscore").val());
+	$(".AwayMinus").on(isMobile() ? 'touchend' : 'click', function() {
+		var current = parseInt($("#awayscore").text());
 		if (current === 0){
 		}else{
-			$("#awayscore").val(current - 1);
+			$("#awayscore").text(current - 1);
 		}
 	});
-	$(".AwayPlus").click(function() {
-		var current = parseInt($("#awayscore").val());
+	$(".AwayPlus").on(isMobile() ? 'touchend' : 'click', function() {
+		var current = parseInt($("#awayscore").text());
 		if (current == 999){
 		}else{
-			$("#awayscore").val(current + 1);
+			$("#awayscore").text(current + 1);
 		}
 	});
-	$("#resetscore").click(function() {
-		$("#homescore").val(0);
-		$("#awayscore").val(0);
+	$("#resetscore").on(isMobile() ? 'touchend' : 'click', function() {
+		$("#homescore").text(0);
+		$("#awayscore").text(0);
 	});
 });
 
@@ -290,14 +297,6 @@ function progressZero(id){
 	$(id + ' .pie').attr('style','');
 }
 
-function isMobile() {
-	if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4)) ) {
-		return true;
-	} else {
-		return false;
-	}
-}
-
 $(document).ready(function() {
 	console.log(navigator.userAgent, navigator.userAgent.substr(0,4));
 	$("#homeeuchername").on('keyup',function(){
@@ -333,6 +332,12 @@ $(document).ready(function() {
 
 	$('.home-minus').on(isMobile() ? 'touchend' : 'click', function(){
 		minusHomePoint();
+	});
+
+	$('.btn-expand').on(isMobile() ? 'touchend' : 'click', function(){
+		$(this).find('.fa').toggleClass('fa-plus');
+		$(this).find('.fa').toggleClass('fa-minus');
+		$(this).parent().parent().toggleClass('modal-100');
 	});
 });
 
