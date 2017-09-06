61ead6a3fb223e8578b1ae2395705d14c1e7cce8
Mark Lintern <mark.lintern@oracle.com>
2017-09-06 11:02:15 -0400

    Eucher Score in Local Storage

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
+/*
+ * Local Data
+ */
+var eucher_data = storage.get('marklintern-com-eucher') || {"homeTeamName":"Good Guys", "awayTeamName":"Bad Guys", "homeTeamScore":0, "awayTeamScore":0, "homeTeamWins":0, "awayTeamWins":0};
+if ( whatIsIt(eucher_data) == 'string' ) {
+	eucher_data = JSON.parse(eucher_data);
+}
+storage.save(eucher_storage_name, eucher_data);
+
+
+/*
+ * Helper Functions
+ */
 function isMobile() {
 	if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4)) ) {
 		return true;
@@ -6,7 +19,9 @@ function isMobile() {
 	}
 }
 
-// Timer
+/*
+ * Timer Funcitonality
+ */
 var ms = 0;
 var state = 0;
 var first = 0;
@@ -17,7 +32,7 @@ function showtime() {
 	targettime = ms / 1000;
 	minutefield = Math.floor(targettime / 60);
 	secondfield = (targettime % 60 < 10 ? "0" : "") + targettime % 60;
-	secondfieldForDisplay = targettime % 60; 
+	secondfieldForDisplay = targettime % 60;
 
 	if (minutefield <= 9) {
 		document.images.h.src = "images/numerals/c0.gif";
@@ -261,6 +276,8 @@ function minusHomePoint() {
 	}else{
 		progressDown('.home-progress', currentscore, 1);
 		$("#eucherhomescore").text(currentscore - 1);
+		eucher_data.homeTeamScore = currentscore - 1;
+		storage.save(eucher_storage_name, eucher_data);
 	}
 }
 
@@ -271,11 +288,15 @@ function addHomePoint(num){
 			if ( parseInt($("#eucherhomescore").text()) == ( WIN_POINTS - 1 ) ) {
 				stop = true;
 				$("#eucherhomeoverallscore").val(parseInt($("#eucherhomeoverallscore").val()) + 1);
+				eucher_data.homeTeamWins = $("#eucherhomeoverallscore").val();
+				storage.save(eucher_storage_name, eucher_data);
 				reseteuchergame();
 			} else {
 				currScore = parseInt($("#eucherhomescore").text());
 				progressUp('.home-progress', currScore, 1);
 				$("#eucherhomescore").text(currScore + 1);
+				eucher_data.homeTeamScore = currScore + 1;
+				storage.save(eucher_storage_name, eucher_data);
 			}
 		}
 	}
@@ -287,6 +308,8 @@ function minusAwayPoint() {
 	}else{
 		progressDown('.away-progress', currentscore, 1);
 		$("#eucherawayscore").text(currentscore - 1);
+		eucher_data.awayTeamScore = currentscore - 1;
+		storage.save(eucher_storage_name, eucher_data);
 	}
 }
 
@@ -297,11 +320,15 @@ function addAwayPoint(num){
 			if ( parseInt($("#eucherawayscore").text()) == ( WIN_POINTS - 1 ) ) {
 				stop = true;
 				$("#eucherawayoverallscore").val(parseInt($("#eucherawayoverallscore").val()) + 1);
+				eucher_data.awayTeamWins = $("#eucherawayoverallscore").val();
+				storage.save(eucher_storage_name, eucher_data);
 				reseteuchergame();
 			}else{
 				currScore = parseInt($("#eucherawayscore").text());
 				progressUp('.away-progress', currScore, 1);
 				$("#eucherawayscore").text(currScore + 1);
+				eucher_data.awayTeamScore = currScore + 1;
+				storage.save(eucher_storage_name, eucher_data);
 			}
 		}
 	}
@@ -369,25 +396,51 @@ function progressZero(id){
 	$(id + ' .pie').attr('style','');
 }
 
+function setData(data) {
+	console.log(data);
+	$("#homeeucherteamname").text(data.homeTeamName);
+	$("#homeeuchername").val(data.homeTeamName);
+	$("#awayeucherteamname").text(data.awayTeamName);
+	$("#awayeuchername").val(data.awayTeamName);
+	$("#eucherhomeoverallscore").val(data.homeTeamWins);
+	$("#eucherawayoverallscore").val(data.awayTeamWins);
+	$("#eucherhomescore").text(data.homeTeamScore);
+	$("#eucherawayscore").text(data.awayTeamScore);
+	progressUp('.home-progress', 0, data.homeTeamScore);
+	progressUp('.away-progress', 0, data.awayTeamScore);
+}
+
+var eucher_storage_name = 'marklintern-com-eucher';
+
 $(document).ready(function() {
-	console.log(navigator.userAgent, navigator.userAgent.substr(0,4));
+	setData(eucher_data);
+
 	$("#homeeuchername").on('keyup',function(){
 		$("#homeeucherteamname").text($('#homeeuchername').val());
+		eucher_data.homeTeamName = $('#homeeuchername').val();
+		storage.save(eucher_storage_name, eucher_data);
 		$(".scorecontainer").width($("#homeeucherteamname").width() + 20 + $("#awayeucherteamname").width() + 20 + 135);
 		$(".scorecontainer").css({'margin':'0 auto'});
 	});
-	
+
 	$("#awayeuchername").on('keyup',function(){
 		$("#awayeucherteamname").text($('#awayeuchername').val());
+		eucher_data.awayTeamName = $('#awayeuchername').val();
+		storage.save(eucher_storage_name, eucher_data);
 		var newWidth = $("#homeeucherteamname").width() + 20 + $("#awayeucherteamname").width() + 20 + 135;
 		$(".scorecontainer").width( newWidth );
 		$(".scorecontainer").css({'margin':'0 auto'});
 	});
-	
+
 	$("#eucherresetscore").on(isMobile() ? 'touchend' : 'click', function() {
 		reseteuchergame();
 		$("#eucherhomeoverallscore").val(0);
 		$("#eucherawayoverallscore").val(0);
+		eucher_data.homeTeamWins = 0;
+		eucher_data.awayTeamWins = 0;
+		eucher_data.homeTeamScore = 0;
+		eucher_data.awayTeamScore = 0;
+		storage.save(eucher_storage_name, eucher_data);
 	});
 
 	$('.away-plus').on(isMobile() ? 'touchend' : 'click', function(){
@@ -412,4 +465,3 @@ $(document).ready(function() {
 		$(this).parent().parent().toggleClass('modal-100');
 	});
 });
-
